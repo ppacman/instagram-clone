7c92fbd426be0bf8f2befda4aa677e8f93e5608a
ppacman <smwfkim2@naver.com>
2023-04-15 15:35:04 +0900

    [#2] Add sideBar - 사이드바 파일 생성

---
+++ src/components/sideBar/sideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const sideBar = () => {
+    return (
+        <div>
+         sideBar
+        </div>
+    );
+};
+
+export default sideBar;
