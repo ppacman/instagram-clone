5ba3f09ca07b52b98f4e33ebfb3c0068c96688a7
ppacman <smwfkim2@naver.com>
2023-04-17 13:57:37 +0900

    [#1] 환경설정 -  app.tsx  를 Layout 으로 감싸기

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SideBar from '../sideBar/sideBar';
+
+interface Props {
+  children: React.ReactNode;
+}
+const Layout = ({ children }: Props) => {
+    return (
+        <div>
+            <SideBar />
+        </div>
+    );
+};
+
+export default Layout;
+
+
