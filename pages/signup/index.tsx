16cd3ab2dc1208e7aa4267ca4a623ad7453a3564
maaanzi <mz151@naver.com>
2023-05-11 14:56:30 +0900

    [#0] 초기 화면출력

---
+++ pages/signup/index.tsx
@@ -0,0 +1,8 @@
+import type {NextPage} from "next";
+import SignUpForm from "@/components/layout/signupPage/SignUpForm";
+
+const Sign : NextPage = () => {
+  return <SignUpForm />;
+};
+
+export default Sign;
