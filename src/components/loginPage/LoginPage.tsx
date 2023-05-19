e74fb57f790e164d4ec0fc25c2fdc5f55f80f8bd
ppacman <smwfkim2@naver.com>
2023-05-19 20:35:24 +0900

    [#3] Add loginPage - Wrapper 추가

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 import LoginBox from './components/LoginBox';
+import SignupBox from './components/SignupBox';
 const LoginPage = () => {
     return (
+        <Wrapper>
        <LoginBox />
+       <SignupBox />
+       </Wrapper>
 
       
     );
 };
 
 export default LoginPage;
+
+
+const Wrapper = styled.div`
+   
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+    margin-top: 12px;
+    max-width: 350px
+`
