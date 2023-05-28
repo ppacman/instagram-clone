890dcab0fd9ecb585bec144e1681d66e5ef42bbb
ppacman <smwfkim2@naver.com>
2023-05-28 17:25:06 +0900

    [#2] Add sideBar - sideBarPage 추가

---
+++ pages/sideBar/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import SideBar from "@/components/sideBar/SideBar";
+
+
+const SideBarPage : NextPage = () =>{
+    return <SideBar /> ;
+};
+
+export default SideBarPage;
+
+  
+
+
