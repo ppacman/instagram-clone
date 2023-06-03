4e35bf7cb33ebd6badac4731fece8655adf16d9a
maaanzi <mz151@naver.com>
2023-06-04 01:23:29 +0900

    전반적 ui 수정 ( 성공, 실패 자동으로 바뀌는 것 구현 X )

--- src/components/signUpPage/signUpForm.tsx
+++ src/components/signUpPage/signUpForm.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { FormEvent, useCallback, useState } from 'react';
 import styled from 'styled-components';
 import logo from '../signUpPage/logo.png';   //이거는 이제 너  이미지 파일이 예를 들어    image.png 면  image 만 써서 import 하면 되고 이미지 파일은 
 // public 폴더에 image 파일에 담아놓으면 돼 
@@ -16,23 +16,41 @@ function SignUpForm() {
     visible : false,
   })
 
-  const [isEmail, setIsEmail] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
   const [emailMessage, setEmailMessage] = useState('');
 
   // submit 이벤트 핸들러
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // 서버로 회원가입 데이터 전송
-  };
+  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   // 서버로 회원가입 데이터 전송
+  // };
 
   // 입력 값이 변경될 때마다 상태값 업데이트
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const emailRegExp = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    if(!emailRegExp) {
-      return(false)
-    }
     setEmailOrPhone(e.target.value);
+    console.log(validateEmail(emailOrPhone))
+    const check = validateEmail(emailOrPhone);
   };
+  const handleSignUp = (e:FormEvent) => {
+    e.preventDefault();
+    const isValid = validateEmail(emailOrPhone);
+    setIsEmailValid(isValid);
+  };
+  const validateEmail = (emailOrPhone:string):boolean => {
+    const emailRegExp = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ || /^\d{3}-\d{3,4}-\d{4}$/; // 이메일 or 전화번호
+    return emailRegExp.test(emailOrPhone);
+  };
+    // const emailRegExp = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    // const emailCheck = (emailOrPhone:string) => {
+    //   console.log(emailRegExp.test(emailOrPhone))
+    //   return emailRegExp.test(emailOrPhone);
+    // }
+
+
+
+
+
+
   const handleFullnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFullname(e.target.value);
   };
@@ -45,7 +63,7 @@ function SignUpForm() {
     // if(!passwordRegExp) {
     //   return(console.log("false"))
     // }
-    // setPassword(e.target.value);
+    setPassword(e.target.value);
   };
   const handlePasswordType = () => {
     setPasswordType(() => {
@@ -64,8 +82,7 @@ function SignUpForm() {
  
 
   return (
-    // <form onSubmit={handleSubmit}>
-    <Form>
+    <Form onSubmit={handleSignUp}>
       <Box>
         <Image src={logo} alt ={""}  width={200} height={80} />    
         <Title>
@@ -75,6 +92,7 @@ function SignUpForm() {
         <ContainerStyle><TextStyle>또는</TextStyle></ContainerStyle> */}
         <Email>
           <Input type="email" value={emailOrPhone} placeholder='휴대폰 번호 또는 이메일 주소' onChange={handleEmailChange} required></Input>
+          {{isEmailValid} ? <Check>성공</Check> : <Check>실패</Check>}
         </Email>
         <Fullname>
           <Input type="text" value={fullname} placeholder="성명" onChange={handleFullnameChange} required></Input>
@@ -102,19 +120,20 @@ export default SignUpForm;
 const Form = styled.div`
   display: flex;
   flex-direction: column;
-  width: auto;
-  height: auto;
   align-items: center;
   justify-content: center;
-  `;
+  min-height: 100vh;
+`;
+
 const Box = styled.div`
-display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 400px;
   height: 500px;
   border: 1px solid;
+  border-color: #b2bec3;
 
 `;
 
@@ -132,8 +151,8 @@ font-weight: bold;
 
 const CheckPw = styled.button`
   position: absolute;
-  left: 360px;
-  top: 360px;
+  right: 510px;
+  top: 440px;
   font-weight: bold;
   /* width: auto;
   height: 38px; */
@@ -142,7 +161,12 @@ const CheckPw = styled.button`
   &:hover {
     color: grey;
   }
-  
+`;
+
+const Check = styled.span`
+  position: absolute;
+  right: 510px;
+  top: 278px;
 `;
 
 // const RanButton = styled.button`
